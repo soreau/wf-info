18cc857ddc17d545013657121078a9e339d3c45f
Scott Moreau <oreaus@gmail.com>
2024-08-31 21:26:06 -0600

    scripts: Output pretty json format

--- ipc-scripts/wf-info.py
+++ ipc-scripts/wf-info.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 
+import json
 from wayfire import WayfireSocket
 from wayfire.extra.wpe import WPE
 
 sock = WayfireSocket()
 wpe = WPE(sock)
 
-print(f"View Info: {wpe.get_view_info()}")
+print(f"View Info:\n{json.dumps(wpe.get_view_info(), indent=2)}")
